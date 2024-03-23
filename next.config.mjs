7f8578ad549273cd97b66d17ba8315c4afffdcde
Daniel Friesen <d@danf.ca>
2024-03-23 09:34:14 -0700

    Install next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import nextPWA from 'next-pwa'
+const withPWA = nextPWA({
+	dest: 'public'
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
