6a72422aa196846dfd4edc8e2d27c022e4b54fa8
Daniel Friesen <d@danf.ca>
2024-03-24 19:59:37 -0700

    Add a debug line to retry to prepare for catching rate limit errors

--- src/framework/query/client.ts
+++ src/framework/query/client.ts
@@ -1,13 +1,20 @@
 import { QueryClient } from '@tanstack/react-query';
 import 'client-only';
 
+function retry(failureCount: number, error: unknown) {
+	console.debug(error);
+	return false;
+}
+
 export const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
 			throwOnError: true,
+			retry,
 		},
 		mutations: {
 			throwOnError: true,
+			retry,
 		},
 	},
 });
